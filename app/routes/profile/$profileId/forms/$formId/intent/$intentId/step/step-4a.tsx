5b18aa3c2d203d2b2f9727be6db61dfb029c8889
MarkMutt <lleasy8@gmail.com>
2023-03-13 17:19:55 -0400

    uploading removes nav buttons

--- app/routes/profile/$profileId/forms/$formId/intent/$intentId/step/step-4a.tsx
+++ app/routes/profile/$profileId/forms/$formId/intent/$intentId/step/step-4a.tsx
@@ -348,19 +348,26 @@ export default function Step4a() {
 
 
         <div className="py-3 flex justify-end">
+          {
+            isUploading ? <p>Please wait for upload to finish</p>:<>
+            
+
       <Link
         to={backUrl}
         type="button"
+        
         className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
+        >
         Previous
       </Link>
       <Link
         to={nextUrl}
         className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
+        >
         Save/Next
       </Link>
+        </>
+      }
     </div>
 
       </div>
