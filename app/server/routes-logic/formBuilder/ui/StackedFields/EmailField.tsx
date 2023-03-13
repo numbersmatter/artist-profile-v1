a492cf2a4e0a277731d35553d8c1c3976be33d6c
MarkMutt <lleasy8@gmail.com>
2023-03-13 11:10:13 -0400

    add css to make all fields visible on mobile

--- app/server/routes-logic/formBuilder/ui/StackedFields/EmailField.tsx
+++ app/server/routes-logic/formBuilder/ui/StackedFields/EmailField.tsx
@@ -2,21 +2,22 @@
 
 
 
-export default function TextField(
+export default function EmailField(
   props:{
     errorText? :string,
     fieldId: string,
     defaultValue: string,
     label: string,
+    placeholder?:string,
   }
 ) {
 
   const inputErrorCss = 
-  "block w-full bg-gray-50 rounded-md border-2 py-1.5 px-3 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+  "block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
 
 
 
-  const inputCss ="block  py-1.5  bg-gray-50 w-full rounded-md px-3 border-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+  const inputCss ="block w-full rounded-md px-3 border-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
   const labelCss = "block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2";
   const labelErrorCss = "block text-sm font-medium text-red-500 sm:mt-px sm:pt-2"
 
@@ -34,7 +35,6 @@ export default function TextField(
           name={props.fieldId}
           className={boxCss}
           defaultValue={props.defaultValue}
-          placeholder="Enter Text Here"
         />
       </div>
       {
--- app/server/routes-logic/formBuilder/ui/StackedFields/TextArea.tsx
+++ app/server/routes-logic/formBuilder/ui/StackedFields/TextArea.tsx
@@ -21,7 +21,7 @@ export default function TextAreaField(
           id={props.fieldId}
           name={props.fieldId}
           rows={3}
-          className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+          className=" block w-full rounded-md border-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
           defaultValue={props.defaultValue}
         />
       </div>
